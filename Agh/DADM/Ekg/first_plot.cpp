7909dae9454ec0191dc0021bb09704700b1f2548
Malgorzata Gora <gora.malgorzata.b@gmail.com>
2014-12-07 19:39:15 +0100

    proba4.

---
+++ Agh/DADM/Ekg/first_plot.cpp
@@ -0,0 +1,20 @@
+#include "ekg.h"
+#include <QtWidgets/QApplication>
+#include <iostream>
+#define FFTW_DLL
+#include <fftw3.h>
+#include <gsl\gsl_sf_bessel.h>
+#include <qdebug.h>
+#include <qapplication.h>
+#include <qwt_plot.h> 
+#include <qwt_plot_curve.h>
+#include <qwt_plot_grid.h>
+#include <qwt_symbol.h>
+#include <qwt_legend.h>
+#include <wfdb.h>
+
+
+int main(int argc, char *argv[])
+{
+
+}
