67bed55cc1bb5ad9d7e1d145b83a587bc514af52
AnnaCzesak <anczesak@gmail.com>
2015-01-11 00:34:08 +0100

    update calculationsVCG.cpp

--- Agh/DADM/Ekg/calculationsVCG.cpp
+++ Agh/DADM/Ekg/calculationsVCG.cpp
@@ -1,5 +1,4 @@
 #include <vector>
-#include <map>
 #include <math.h>
 #include <dataVCG.h>
 
@@ -8,9 +7,8 @@ using namespace std;
 int main (){
 	
 int HeartbeatsCtr;
-int HeartbeatsAmount= sizeof(AllX);    
+int HeartbeatsAmount= sizeof(QRS_ONSET);    
 double ElAz_Sum = 0; // We will need to sum all DEA to calculate the DEA mean
-
 
 vector <double> All_T_AxisVectorsX;   //Init array containing T endpoints coordinates
 vector <double> All_T_AxisVectorsY;
@@ -28,75 +26,78 @@ vector < double >::iterator wsk_DEA_EachLoopMeans = DEA_EachLoopMeans.begin();
 
 
 //-----Znajdümy punkt izoelektryczny w ca≥ym VCG:---------------------------
-//vector <double> Z; 
-//for (HeartbeatsCtr = 0;  HeartbeatsCtr < AllSignalLength; HeartbeatsCtr++)
-//{
-//    Z[HeartbeatsCtr] = sqrt( AllX(HeartbeatsCtr)^2 + AllY(HeartbeatsCtr)^2 + AllZ(HeartbeatsCtr)^2);
-//}
+vector <double> Z; 
+double IsoelectricPoint[3]= {AllX[0],AllY[0],AllZ[0]};
+double minZ= sqrt(AllX[0]*AllX[0] + AllY[0]*AllY[0] + AllZ[0]*AllZ[0]);  // Z[0]
 
-//[Nth, IsoelectricPoint_Idx] = min(Z);
-//IsoelectricPoint = [AllX(IsoelectricPoint_Idx), AllY(IsoelectricPoint_Idx), AllZ(IsoelectricPoint_Idx)];
-
-//TempDimention = size(QRS_ONSET);
-//HeartbeatsAmount = TempDimention(1);
+for (HeartbeatsCtr = 1;  HeartbeatsCtr < AllSignalLength+1; HeartbeatsCtr++){
+	Z[HeartbeatsCtr] = sqrt(AllX[HeartbeatsCtr]*AllX[HeartbeatsCtr] + AllY[HeartbeatsCtr]*AllY[HeartbeatsCtr] + AllZ[HeartbeatsCtr]*AllZ[HeartbeatsCtr]);
+	if (Z[HeartbeatsCtr-1]>Z[HeartbeatsCtr] & minZ>Z[HeartbeatsCtr]){
+		IsoelectricPoint[0,1,2]=AllX[HeartbeatsCtr],AllY[HeartbeatsCtr],AllZ[HeartbeatsCtr];
+	    minZ = Z[HeartbeatsCtr];
+	}
+}
 
 //==============================================================================================================
 
-for (HeartbeatsCtr = 0;  HeartbeatsCtr < HeartbeatsAmount; HeartbeatsCtr++)
-{
+for (HeartbeatsCtr = 0;  HeartbeatsCtr < HeartbeatsAmount; HeartbeatsCtr++){
+
+	double LoopPointsCtr;
+// ------------------------- T-LOOP -----------------------------------------
 	double T_MaxModuleValue = 0; //Init maximum T loop's module value
     double T_ModulesSum = 0; //Init sum needed to count up the mean value (needed in RMMV)
 	double dzielnik = 0;
 	double T_AxisVector[3]={0,0,0};
+// ------------------------- QRS-LOOP -----------------------------------------
+    double QRS_AxisVector[3] = {0,0,0}; //Init QRS loop axis vectors
+    double QRS_MaxModuleValue = 0; //Init maximum QRS loop's module value
+    double X,Y,Z; 
+	double ThisPointModule;
 
+// ------------------------- T-LOOP -----------------------------------------
     
-	for (double TLoopPointsCtr = T_ONSET[HeartbeatsCtr]; TLoopPointsCtr < T_END[HeartbeatsCtr]; TLoopPointsCtr++)
-	{
-	//double This_Heartbeat_DEAs_Sum = 0; //We will need to sum all DEA to calculate the DEA mean
-    double ThisPointVector [3];
-	double ThisPointLSVector [2];
-	double Z_axis_LSVector [2] = {0,1}; //[Y,Z]
-	double X_axis_FVector [2] = {1,0}; //[X,Y]
-	double Elevation;
-	double normZ_axis_LSVector=1, normX_axis_FVector=1;  //!!!!!!!!!!!!!!!!!!!
-	double norm_ThisPointLSVector=0.0667; //norm(ThisPointLSVector) !!!!!!!!!!!!!!!!obliczyc funkcja norm(ThisPointLSVector)
-	double norm_ThisPointFVector=0.0371;//!!!!!!!!!!!!!!!!!!!!
-	double Azimuth;
-	double ThisPointModule;   
-	double dot1;
+	for (LoopPointsCtr = T_ONSET[HeartbeatsCtr]; LoopPointsCtr < T_END[HeartbeatsCtr]; LoopPointsCtr++)	{
+	
+		double ThisPointVector [3];
+		double ThisPointLSVector [2];
+		double Z_axis_LSVector [2] = {0,1}; //[Y,Z]
+		double X_axis_FVector [2] = {1,0}; //[X,Y]
+		double ThisPointFVector [2] = {0,0};
+		double Elevation;
+		double Azimuth;
+		double ThisPointModule;   
+		double dot1;
 
+// ------------------------- T-LOOP -----------------------------------------
+		X = AllX[LoopPointsCtr];
+		Y = AllY[LoopPointsCtr];
+		Z = AllZ[LoopPointsCtr];
 
-
-	// ------------------------- T-LOOP -----------------------------------------
-	double X = AllX[TLoopPointsCtr];
-	double Y = AllY[TLoopPointsCtr];
-	double Z = AllZ[TLoopPointsCtr];
-
-	ThisPointModule = sqrt(pow(X-IsoelectricPoint[0],2) + pow(Y-IsoelectricPoint[1],2) + pow(Z-IsoelectricPoint[2],2)) ;
+		ThisPointModule = sqrt(pow(X-IsoelectricPoint[0],2) + pow(Y-IsoelectricPoint[1],2) + pow(Z-IsoelectricPoint[2],2)) ;
 	 
-	if (ThisPointModule>T_MaxModuleValue)
-		{
-			T_MaxModuleValue = ThisPointModule;
-			T_AxisVector[0] = AllX[TLoopPointsCtr];
-			T_AxisVector[1] = AllY[TLoopPointsCtr];
-			T_AxisVector[2] = AllZ[TLoopPointsCtr];
+		if (ThisPointModule>T_MaxModuleValue){
+
+				T_MaxModuleValue = ThisPointModule;
+				T_AxisVector[0] = AllX[LoopPointsCtr];
+				T_AxisVector[1] = AllY[LoopPointsCtr];
+				T_AxisVector[2] = AllZ[LoopPointsCtr];
 		}
-        T_ModulesSum = T_ModulesSum + ThisPointModule; //(needed in RMMV)
+		T_ModulesSum = T_ModulesSum + ThisPointModule; //(needed in RMMV)
         
         //Meanwhile do some calculcations for DEA:
-        ThisPointVector[0] = AllX[TLoopPointsCtr];
-		ThisPointVector[1] = AllY[TLoopPointsCtr]; 
-		ThisPointVector[2] = AllZ[TLoopPointsCtr];
+        ThisPointVector[0] = AllX[LoopPointsCtr];
+		ThisPointVector[1] = AllY[LoopPointsCtr]; 
+		ThisPointVector[2] = AllZ[LoopPointsCtr];
 		ThisPointLSVector[0,1] = ThisPointVector[2], ThisPointVector[3]; //[Y,Z], LS - Left Sagittal plane
         
 		
 		dot1 = dot(ThisPointLSVector,Z_axis_LSVector);
-        Elevation = acos(dot1/norm_ThisPointLSVector/normZ_axis_LSVector); 
+        Elevation = acos(dot1/norm(ThisPointLSVector)/norm(Z_axis_LSVector)); 
 
-		double ThisPointFVector [2] = {ThisPointVector[0], ThisPointVector[1]}; //[X,Y], F - Frontal plane
+		ThisPointFVector [0,1]= ThisPointVector[0], ThisPointVector[1]; //[X,Y], F - Frontal plane
    
 		dot1 = dot(ThisPointFVector,X_axis_FVector);
-        Azimuth = acos(dot1/norm_ThisPointFVector/normX_axis_FVector); 
+        Azimuth = acos(dot1/norm(ThisPointFVector)/norm(X_axis_FVector)); 
         ElAz_Sum = ElAz_Sum + abs(Elevation-Azimuth); //as DEA = mean(abs(El-Az)    
         dzielnik = dzielnik+1;   
    	}
@@ -108,67 +109,63 @@ for (HeartbeatsCtr = 0;  HeartbeatsCtr < HeartbeatsAmount; HeartbeatsCtr++)
     T_LoopsModulesMeans[HeartbeatsCtr] = T_ModulesSum/(T_END[HeartbeatsCtr]-T_ONSET[HeartbeatsCtr]+1); //(needed in RMMV)
     DEA_EachLoopMeans[HeartbeatsCtr] = ElAz_Sum/dzielnik;  // (needed in DEA)
    
-// ------------------------- QRS-LOOP -----------------------------------------
-    
-	map <char,double> QRS_AxisVector; //Init QRS loop axis vectors
-	  QRS_AxisVector['x'] = 0;
-      QRS_AxisVector['y'] = 0;
-      QRS_AxisVector['z'] = 0;
-    double QRS_MaxModuleValue = 0; //Init maximum QRS loop's module value
-    
-	for (double QRSLoopPointsCtr = QRS_ONSET[HeartbeatsCtr]; QRSLoopPointsCtr<QRS_END[HeartbeatsCtr]; QRSLoopPointsCtr++)
-	{
-	double X = AllX[QRSLoopPointsCtr];
-	double Y = AllY[QRSLoopPointsCtr];
-	double Z = AllZ[QRSLoopPointsCtr];
 
-	double ThisPointModule = sqrt(pow(X-IsoelectricPoint[0],2) + pow(Y-IsoelectricPoint[1],2) + pow(Z-IsoelectricPoint[2],2)) ;
+// ------------------------- QRS-LOOP -----------------------------------------	
+    
+	for (LoopPointsCtr = QRS_ONSET[HeartbeatsCtr]; LoopPointsCtr<QRS_END[HeartbeatsCtr]; LoopPointsCtr++){
+		X = AllX[LoopPointsCtr];
+		Y = AllY[LoopPointsCtr];
+		Z = AllZ[LoopPointsCtr];
 
-		 if (ThisPointModule>QRS_MaxModuleValue)
-			{
+		ThisPointModule = sqrt(pow(X-IsoelectricPoint[0],2) + pow(Y-IsoelectricPoint[1],2) + pow(Z-IsoelectricPoint[2],2)) ;
+
+		 if (ThisPointModule>QRS_MaxModuleValue){
 			    QRS_MaxModuleValue = ThisPointModule;
-				QRS_AxisVector ['x'] = AllX[QRSLoopPointsCtr];
-				QRS_AxisVector ['y'] = AllY[QRSLoopPointsCtr];
-				QRS_AxisVector ['z'] = AllZ[QRSLoopPointsCtr];
-			}
+				QRS_AxisVector [0] = AllX[LoopPointsCtr];
+				QRS_AxisVector [1] = AllY[LoopPointsCtr];
+				QRS_AxisVector [2] = AllZ[LoopPointsCtr];
+		 }
 	}
     
-	
-    All_QRS_AxisVectorsX[HeartbeatsCtr] = QRS_AxisVector['x'];
-	All_QRS_AxisVectorsY[HeartbeatsCtr] = QRS_AxisVector['y'];
-	All_QRS_AxisVectorsZ[HeartbeatsCtr] = QRS_AxisVector['z'];
+	All_QRS_AxisVectorsX[HeartbeatsCtr] = QRS_AxisVector[0];
+	All_QRS_AxisVectorsY[HeartbeatsCtr] = QRS_AxisVector[1];
+	All_QRS_AxisVectorsZ[HeartbeatsCtr] = QRS_AxisVector[2];
 	
 }
 
 
 //Calculate MA parameter for each heartbeat:
 
-//"The angle between two lines is the angle between their direction vectors.
-//The direction vectors of two lines can always be found by subtracting the 
-//position vectors of two points on the line"
-//http://www.mathworks.com/matlabcentral/newsreader/view_thread/276582
-
-vector <double> mMA; //Init MA array !!!!!!!!!!!!!!!!!!!!!!!!!!!?????
+vector <double> mMA; 
 vector < double >::iterator wsk_mMA = mMA.begin();
-double Results_mMA;
-double Results_mMA_std;
+double Results_mMA, Results_mMA_std;
 
-for (HeartbeatsCtr = 0;  HeartbeatsCtr < HeartbeatsAmount; HeartbeatsCtr++)
-{//Calculate axis (as vectors)
-	double TLoopVector[3] = {All_T_AxisVectorsX[HeartbeatsCtr],All_T_AxisVectorsY[HeartbeatsCtr],All_T_AxisVectorsZ[HeartbeatsCtr]};
-	double TLoopVector_XY[2] = {TLoopVector[1], TLoopVector[2]};
-	double TLoopVector_XZ[2] = {TLoopVector[1], TLoopVector[3]};
-	double TLoopVector_ZY[2] = {TLoopVector[2], TLoopVector[3]};
-    double QRSLoopVector[3] =  {All_QRS_AxisVectorsX[HeartbeatsCtr],All_QRS_AxisVectorsY[HeartbeatsCtr],All_QRS_AxisVectorsZ[HeartbeatsCtr]};
-	double QRSLoopVector_XY[2] = {QRSLoopVector[1], QRSLoopVector[2]};
-	double QRSLoopVector_XZ[2] = {QRSLoopVector[1], QRSLoopVector[3]};
-	double QRSLoopVector_ZY[2] = {QRSLoopVector[2], QRSLoopVector[3]};
+double TLoopVector_XY[2];
+double TLoopVector_XZ[2];
+double TLoopVector_ZY[2];
+
+double QRSLoopVector_XY[2];
+double QRSLoopVector_XZ[2];
+double QRSLoopVector_ZY[2];
+
+double A_F, A_H, A_LS, max;
+
+for (HeartbeatsCtr = 0;  HeartbeatsCtr < HeartbeatsAmount; HeartbeatsCtr++){
+
+	//Calculate axis (as vectors)
+	TLoopVector_XY[0,1,2] = All_T_AxisVectorsX[HeartbeatsCtr], All_T_AxisVectorsY[HeartbeatsCtr];
+	TLoopVector_XZ[0,1,2] = All_T_AxisVectorsX[HeartbeatsCtr], All_T_AxisVectorsZ[HeartbeatsCtr];
+	TLoopVector_ZY[0,1,2] = All_T_AxisVectorsY[HeartbeatsCtr], All_T_AxisVectorsZ[HeartbeatsCtr];
+    
+	QRSLoopVector_XY[0,1,2] = All_QRS_AxisVectorsX[HeartbeatsCtr], All_QRS_AxisVectorsY[HeartbeatsCtr];
+	QRSLoopVector_XZ[0,1,2] = All_QRS_AxisVectorsX[HeartbeatsCtr], All_QRS_AxisVectorsZ[HeartbeatsCtr];
+	QRSLoopVector_ZY[0,1,2] = All_QRS_AxisVectorsY[HeartbeatsCtr], All_QRS_AxisVectorsZ[HeartbeatsCtr];
     //Find angles between two axis in three planes
-    double A_F = acos(dot(TLoopVector_XY,QRSLoopVector_XY)/norm(TLoopVector_XY)/norm(QRSLoopVector_XY)); 
-    double A_H = acos(dot(TLoopVector_XZ,QRSLoopVector_XZ)/norm(TLoopVector_XZ)/norm(QRSLoopVector_XZ)); 
-    double A_LS = acos(dot(TLoopVector_ZY,QRSLoopVector_ZY)/norm(TLoopVector_ZY)/norm(QRSLoopVector_ZY));
+    A_F = acos(dot(TLoopVector_XY,QRSLoopVector_XY)/norm(TLoopVector_XY)/norm(QRSLoopVector_XY)); 
+    A_H = acos(dot(TLoopVector_XZ,QRSLoopVector_XZ)/norm(TLoopVector_XZ)/norm(QRSLoopVector_XZ)); 
+    A_LS = acos(dot(TLoopVector_ZY,QRSLoopVector_ZY)/norm(TLoopVector_ZY)/norm(QRSLoopVector_ZY));
     //Find the greatest angle
-	double max = 0;
+	max = 0;
 	if (A_F>A_H){max = A_F;}
 	else max= A_H;
 	if (max<A_LS){max=A_LS;}
@@ -185,15 +182,14 @@ Results_mMA_std = stddev(wsk_mMA);
 
 //Variables' values are calculated in MA algorithm:
 
-vector <double> mRMMV; //Init array of RMMVs,  ERROR: Expression must have constant value!!!!!
+vector <double> mRMMV; 
 vector < double >::iterator wsk_mRMMV = mRMMV.begin();
 double Vmax;
 double Vn;  
 double Results_mRMMV;
 double Results_mRMMV_std;
 
-for (HeartbeatsCtr = 0;  HeartbeatsCtr < HeartbeatsAmount; HeartbeatsCtr++)
-{
+for (HeartbeatsCtr = 0;  HeartbeatsCtr < HeartbeatsAmount; HeartbeatsCtr++){
 	Vmax = All_T_AxisModules[HeartbeatsCtr];  
     Vn = T_LoopsModulesMeans[HeartbeatsCtr];  
     mRMMV[HeartbeatsCtr] = Vmax/Vn;
@@ -213,4 +209,4 @@ Results_mDEA = mean(wsk_DEA_EachLoopMeans);
 Results_mDEA_std = stddev(wsk_DEA_EachLoopMeans);
 
 	
-	return 0;}
+return 0;}
--- Agh/DADM/Ekg/dataVCG.h
+++ Agh/DADM/Ekg/dataVCG.h
@@ -26,17 +26,6 @@ vector<double> QRS_END;
 vector<double> T_ONSET;
 vector<double> T_END;
 
-//double T_EdgesIndexes[13][2] = {{1,580},{801,1300},{1551,2060},{2251,2780},{2951,3520},{3701,4260},{4451,5000},{5171,5750},{5901,6480},{6651,7200},{7401,7930},{8101,8670},{8850,9390}};
-
-//double QRS_EdgesIndexes[13][2] = {{581,800},{1301,1550},{2061,2250},{2781,2950},{3521,3700},{4261,4450},{5001,5170},{5751,5900},{6481,6650},{7201,7400},{7931,8100},{8671,8850},{9391,9550}};
-    //float pi=3.1415;
-	//for (i=0; i<=arrayset; i++){
-	//	a[i]= pi*(pow(static_cast<float>(g[i]),2));
-	//}
-
-//method dot: dot(ThisPointFVector,X_axis_FVector)
-//dot1 = ();
-
 double dot(double *vector1, double *vector2){
 	int length = sizeof(vector1);
 	double a=0;
