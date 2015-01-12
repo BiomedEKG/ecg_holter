82ea6449dae3ed36eec23369b8661060f15aa2e9
Monika <nika.zakrzewska@gmail.com>
2015-01-12 22:01:20 +0100

    SavitzkyGolay completed.

--- Agh/DADM/Ekg/SavitzkyGolay.cpp
+++ Agh/DADM/Ekg/SavitzkyGolay.cpp
@@ -3,13 +3,15 @@
 //coefficients taken from MATLAB, for 4-order polynominal savitzkygolay filter
 const double SavitzkyGolay::coefficientsArray[] = {01417, 0.0, -0.0043, 0.0, 0.0};
 const unsigned int SavitzkyGolay::span = 25;
+const unsigned int spanHalf = 12;
 const unsigned int SavitzkyGolay::polynominalOrder = 4;
 
 std::vector <double> SavitzkyGolay::calculateSavitzkyGolay (std::vector <double> *signal){
 
 	//std::vector<double> coefficientsVector(coefficientsArray, coefficientsArray + sizeof(coefficientsArray) / sizeof(coefficientsArray[0]) );
+	
 
-	double sumSignal;
+	double sumSignal = 0.0;
 
 	// count average of signal
 	for (unsigned int n = 0; n < inputSignal.size(); n++){
@@ -20,42 +22,69 @@ std::vector <double> SavitzkyGolay::calculateSavitzkyGolay (std::vector <double>
 	averageWholeSignal = sumSignal / inputSignal.size();
 
 	//add samples before first and after last sample because otherway it would be impossible to filter these samples
-	for (unsigned int a = 0; a < (span-1)/2 ; a++){
+	for (unsigned int a = 0; a < spanHalf ; a++){
 		
 		inputSignal.push_back(averageWholeSignal);
 		inputSignal.insert(inputSignal.begin(), averageWholeSignal);
 	}
 
-	spanCount = createSpan();
-	/*
-	//count coefficients of Savitzky-Golay filter
-	double coefficient;
+	createSpan();
+	calculateFilterCoefficients();
 	
-	for (int b = 0; b < span; b++){	
 
-		double sumCoefficient;
-
-		for(int c = 0; c < polynominalOrder+1 ; c++){
-			
-			sumCoefficient = pow();
-		}
-		coefficient = 
-		coefficientsFilter.push_back(); 
-	}
-	return *signal;
-
-}
-*/
-
-std::vector<int> SavitzkyGolay::createSpan(){
-	
-	std::vector<int> createSpanOutput;
-	createSpanOutput.push_back(0);
-
-	for (int unsigned i = 0; i<(span-1)/2; i ++){
+	//filter signal
+	for (unsigned long int j = spanHalf; j < (inputSignal.size() - spanHalf) ; j++ ){
 		
-		createSpanOutput.insert(createSpanOutput.begin(), (-i));
-		createSpanOutput.push_back(i);
+		double sum = inputSignal[j];
+		for(unsigned int i = 1 ; i <= spanHalf; i++){
+
+			sum += inputSignal[j-i] * coefficientsFilter[spanHalf-i];
+			sum += inputSignal[j+i] * coefficientsFilter[spanHalf+i];
+		}
+		outputSignal.push_back(sum);
 	}
-	return createSpanOutput;
+
+	return outputSignal;
+}
+
+
+void SavitzkyGolay::createSpan(){
+	
+	spanCount.push_back(0.0);
+
+	for (int unsigned i = 0; i<spanHalf; i ++){
+		
+		spanCount.insert(spanCount.begin(), (-i));
+		spanCount.push_back(i);
+	}
+}
+
+void SavitzkyGolay::calculateFilterCoefficients(){
+	
+	//calculate coefficients of Savitzky-Golay filter
+
+	//calculate 0 coefficient (middle)
+	double zeroCoefficient = 0.0;
+
+	for(int c = 0; c <= polynominalOrder ; c++){
+			
+		zeroCoefficient += pow(spanCount[spanHalf], c)*coefficientsArray[c];
+	}
+	coefficientsFilter.push_back(zeroCoefficient);
+
+	//calculate others coefficients
+	for (int b = 0; b <= spanHalf; b++){	
+
+		double coefficientL = 0.0;
+		double coefficientR = 0.0;
+
+		for(int c = 0; c <= polynominalOrder ; c++){
+			
+			coefficientL += pow(spanCount[spanHalf -1 -b], c) * coefficientsArray[c];
+			coefficientR += pow(spanCount[spanHalf +1 -b], c) * coefficientsArray[c];
+		}
+		
+		coefficientsFilter.push_back(coefficientR);
+		coefficientsFilter.insert(coefficientsFilter.begin(),coefficientL);
+	}
 }
--- Agh/DADM/Ekg/SavitzkyGolay.h
+++ Agh/DADM/Ekg/SavitzkyGolay.h
@@ -8,15 +8,16 @@ class SavitzkyGolay{
 public:
 		static const double coefficientsArray[];
 		static const unsigned int span;
+		static const unsigned int spanHalf;
 		static const unsigned int polynominalOrder;
 
-		std::vector <double> calculateSavitzkyGolay (std::vector <double>*);
-		
-		std::vector<int> createSpan();
+		std::vector<double> calculateSavitzkyGolay (std::vector <double>*);
+		void createSpan();
+		void calculateFilterCoefficients();
 
 private:
 		std::vector<double> inputSignal;
-		std::vector <double> outputSignal;
+		std::vector<double> outputSignal;
 
 		double averageWholeSignal;
 		std::vector<int> spanCount;
