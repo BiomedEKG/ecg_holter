c366b46ee1e70943abd8ac18cd1d1760533e44ec
AnnaCzesak <anczesak@gmail.com>
2015-01-13 20:54:31 +0100

    add dower.cpp

--- Agh/DADM/Ekg/calculationsVCG.cpp
+++ Agh/DADM/Ekg/calculationsVCG.cpp
@@ -2,6 +2,7 @@
 #include <math.h>
 #include <dataVCG.h>
 
+
 using namespace std;
 
 int main (){
---
+++ Agh/DADM/Ekg/dower.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+#include <math.h>
+#include <dataVCG.h>
+#include <map>
+
+int main (){
+
+map<char, vector<double>> DowerTransform ( vector<double> i, vector<double> ii, vector<double> v1, vector<double> v2, vector<double> v3, vector<double> v4, vector<double> v5, vector<double> v6)
+{
+//Column order = t i ii iii avr avl avf v1 v2 v3 v4 v5 v6 x y z 
+
+map <char, vector<double>> VCG;
+VCG['x'];
+VCG['y'];
+VCG['z'];
+vector <double> VCG_X;
+vector <double> VCG_Y;
+vector <double> VCG_Z;
+
+for (int iter = 0; iter<end; iter++){
+	VCG_X [iter]= (-0.172*i[iter]) + (-0.074*ii[iter]) + (0.122*v1[iter]) + (0.231*v2[iter]) + (0.239*v3[iter]) + (0.194*v4[iter]) +  (0.156*v5[iter]) + (-0.010*v6[iter]);
+	VCG_Y [iter]= (0.057*i[iter]) + (-0.019*ii[iter]) + (-0.106*v1[iter]) + (-0.022*v2[iter]) + (0.041*v3[iter]) + (0.048*v4[iter]) +  (-0.227*v5[iter]) + (0.887*v6[iter]);
+	VCG_Z [iter]= (-0.229*i[iter]) + (-0.310*ii[iter]) + (-0.246*v1[iter]) + (-0.063*v2[iter]) + (0.055*v3[iter]) + (0.108*v4[iter]) +  (0.022*v5[iter]) + (0.102*v6[iter]);
+}
+
+VCG['x'] = VCG_X;
+VCG['y'] = VCG_Y;
+VCG['z'] = VCG_Z;
+
+return VCG;
+}
+
+return 0;
+}
