17fdbb705c7b28407ea1b41314e5a336f5a6baa5
Agata <agata.pyka@tlen.pl>
2015-01-14 22:57:04 +0100

    male zmianki

--- Agh/DADM/TWA1/TWA1/TWA1.cpp
+++ Agh/DADM/TWA1/TWA1/TWA1.cpp
@@ -1,6 +1,3 @@
-// TWA1.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
 #include "io_ecg_file.h"
 
@@ -18,21 +15,12 @@ void msg( const char * m ) {
 
 int main(int argc, _TCHAR* argv[])
 {
-	//testing directories & files
-	//project directory
-	//string direct="d:\\Ulastudia\\DedAlgDiagMed\\Projekt_uporzadkowany\\TWA1\\";
 	string direct="..\\";
-	//input signal dir & file
-	//string insig="signals\\twa00f.txt";
-	string insig="signals\\s0010_0_signal.txt";
-	//input twaves dir & file
-	string intwaves="signals\\s0010_0_t_peaks.txt";
-	//output coefficients - ARy!!!!
-	string outcoeff="results\\s0010_6_res.txt";
-	//fft result - FFT !!!
-	string fftres="results\\s0010_6_fft.txt";
+	string insig="signals\\twa00f.txt";
+	string intwaves="signals\\twaves00.txt";
+	string outcoeff="results\\twa00res.txt";
+	string fftres="results\\twa00fft.txt";
 
-	//Reading filtered signal from file, using function from io_ecg_file.h
 	msg( "start reading file" );
 	restype sig=ReadFile(direct+insig);
 	msg( "finished reading file" );
@@ -40,7 +28,8 @@ int main(int argc, _TCHAR* argv[])
 	int c=sig.channels;
 	vector<string> input=sig.words;
 
-	//Converting signal in text format to doubles & selecting only one channel
+	// Konwertowanie sygna³u z próbek .txt na format double
+
 	double temp;
 	int i;
 	if(c==1)
@@ -53,13 +42,13 @@ int main(int argc, _TCHAR* argv[])
 		samples.push_back(temp);
 	};
 
-	//Reading numbers of t-wave samples from file, using function from io_ecg_file.h
 	restype samp=ReadFile(direct+intwaves);
 	vector<unsigned int> tsamples;
 	c=samp.channels;
 	input=samp.words;
 
-	//Converting signal in text format to doubles & selecting only one channel
+	// Konwertowanie sygna³u z próbek .txt na format double
+
 	if(c==1)
 		i=0;
 	else
@@ -70,29 +59,18 @@ int main(int argc, _TCHAR* argv[])
 		tsamples.push_back(temp);
 	};
 	
-	//samples - amplitudy ekg
-	//tsamples - próbki za³amka T 
-		
-
-	//New t_alt object
+	
 	t_alt TALT=t_alt::t_alt();
-	//Setting value of input signal (filtered by baseline module) to object
 	TALT.set_signal(samples);
-	//Setting value for vector with t-wave samples
 	TALT.set_tsamples(tsamples);
 	TALT.alt_coeff();
 	
-	// stare wyjœcie AR
-	//ofstream outtwa(direct+outcoeff);
-	//for(int i=0;i<TALT.get_result().size();i++)
-	//	outtwa<<TALT.get_result()[i]<<endl;
-	//outtwa.close();
-
 	// wyjœcie AR
 	ofstream outtwa(direct+outcoeff);
-	outtwa<<TALT.get_result();
+	for(int i=0;i<TALT.get_result().size();i++)
+		outtwa<<TALT.get_result()[i]<<endl;
 	outtwa.close();
-	
+
 	// wyjœcie FFT
 	ofstream outfft(direct+fftres);
 	msg( std::string("path: " + direct + fftres ).c_str() );
