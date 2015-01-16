e085816627fa94754c46173704457abde20b17cf
Malgorzata Gora <gora.malgorzata.b@gmail.com>
2015-01-16 21:59:14 +0100

    + wykres 3D VCG (input: 3 x QVector<double>)

---
+++ Agh/DADM/Ekg/include/opengl/CAxis.h
@@ -0,0 +1,96 @@
+/* +---------------------------------------------------------------------------+
+   |                     Mobile Robot Programming Toolkit (MRPT)               |
+   |                          http://www.mrpt.org/                             |
+   |                                                                           |
+   | Copyright (c) 2005-2015, Individual contributors, see AUTHORS file        |
+   | See: http://www.mrpt.org/Authors - All rights reserved.                   |
+   | Released under BSD License. See details in http://www.mrpt.org/License    |
+   +---------------------------------------------------------------------------+ */
+#ifndef opengl_CAxis_H
+#define opengl_CAxis_H
+
+#include <CRenderizableDisplayList.h>
+
+namespace mrpt
+{
+	namespace opengl
+	{
+
+
+		// This must be added to any CSerializable derived class:
+		DEFINE_SERIALIZABLE_PRE_CUSTOM_BASE_LINKAGE( CAxis, CRenderizableDisplayList, OPENGL_IMPEXP )
+
+		/** Draw a 3D world axis, with coordinate marks at some regular interval
+		  *  \sa opengl::COpenGLScene
+		  *  
+		  *  <div align="center">
+		  *  <table border="0" cellspan="4" cellspacing="4" style="border-width: 1px; border-style: solid;">
+		  *  <tr> <td> mrpt::opengl::CAxis </td> <td> \image html preview_CAxis.png </td> </tr>
+		  *  </table>
+		  *  </div>
+		  *  
+		  * \ingroup mrpt_opengl_grp
+		  */
+		class OPENGL_IMPEXP CAxis : public CRenderizableDisplayList
+		{
+			DEFINE_SERIALIZABLE( CAxis )
+		protected:
+			float	m_xmin,m_ymin,m_zmin;
+			float	m_xmax,m_ymax,m_zmax;
+			float	m_frecuency;
+            float	m_lineWidth;
+			bool	m_marks;
+
+		public:
+			void setAxisLimits(float xmin,float ymin, float zmin, float xmax,float ymax, float zmax)
+			{
+				m_xmin=xmin; m_ymin=ymin; m_zmin=zmin;
+				m_xmax=xmax; m_ymax=ymax; m_zmax=zmax;
+				CRenderizableDisplayList::notifyChange();
+			}
+
+			void setFrequency(float f) { ASSERT_(f>0); m_frecuency=f; CRenderizableDisplayList::notifyChange(); } //!< Changes the frequency of the "ticks"
+
+			void setLineWidth(float w) { m_lineWidth=w; CRenderizableDisplayList::notifyChange(); }
+			float getLineWidth() const { return  m_lineWidth;}
+
+			void enableTickMarks(bool v=true) { m_marks=v; CRenderizableDisplayList::notifyChange(); }
+
+
+			/** Class factory  */
+			static CAxisPtr Create(
+				float xmin,float ymin, float zmin,
+				float xmax, float ymax,  float zmax,
+				float frecuency = 1, float lineWidth = 3, bool marks=false);
+
+			/** Render
+			  */
+			void  render_dl() const;
+
+			/** Evaluates the bounding box of this object (including possible children) in the coordinate frame of the object parent. */
+			virtual void getBoundingBox(mrpt::math::TPoint3D &bb_min, mrpt::math::TPoint3D &bb_max) const;
+
+	private:
+			/** Constructor
+			  */
+			CAxis(
+				float xmin=-1.0f,float ymin=-1.0f, float zmin=-1.0f,
+				float xmax=1.0f, float ymax=1.0f,  float zmax=1.0f,
+				float frecuency = 0.25f, float lineWidth = 3.0f, bool marks=false) :
+				m_xmin(xmin),m_ymin(ymin),m_zmin(zmin),
+				m_xmax(xmax),m_ymax(ymax),m_zmax(zmax),
+				m_frecuency(frecuency),
+                m_lineWidth(lineWidth),
+				m_marks(marks)
+			{
+			}
+
+			/** Private, virtual destructor: only can be deleted from smart pointers */
+			virtual ~CAxis() { }
+		};
+		DEFINE_SERIALIZABLE_POST_CUSTOM_BASE_LINKAGE( CAxis, CRenderizableDisplayList, OPENGL_IMPEXP )
+
+	} // end namespace
+} // End of namespace
+
+#endif
---
+++ Agh/DADM/Ekg/include/opengl/link_pragmas.h
@@ -0,0 +1,94 @@
+/* +---------------------------------------------------------------------------+
+   |                     Mobile Robot Programming Toolkit (MRPT)               |
+   |                          http://www.mrpt.org/                             |
+   |                                                                           |
+   | Copyright (c) 2005-2015, Individual contributors, see AUTHORS file        |
+   | See: http://www.mrpt.org/Authors - All rights reserved.                   |
+   | Released under BSD License. See details in http://www.mrpt.org/License    |
+   +---------------------------------------------------------------------------+ */
+
+#ifndef opengl_link_pragmas_H
+#define opengl_link_pragmas_H
+
+#include <mrpt/config.h>
+#include <mrpt/utils/boost_join.h>
+
+// ** Important! **
+// In each mrpt library, search and replace:
+//  MRPT_XXX_EXPORT, MRPT_XXX_IMPORT
+//  OPENGL_IMPEXP, mrpt_xxx_EXPORTS
+
+// If we are building the DLL (_EXPORTS), do not link against the .lib files:
+#if !defined(mrpt_opengl_EXPORTS) && (defined(_MSC_VER) || defined(__BORLANDC__))
+#	if defined(_DEBUG)
+#		pragma comment (lib, BOOST_JOIN( BOOST_JOIN("libmrpt-opengl",MRPT_VERSION_POSTFIX),"-dbg.lib"))
+#	else
+#		pragma comment (lib, BOOST_JOIN( BOOST_JOIN("libmrpt-opengl",MRPT_VERSION_POSTFIX),".lib"))
+#	endif
+#endif
+
+
+
+
+/*   The macros below for DLL import/export are required for Windows only.
+    Mostly all the definitions in this file are copied or at least opengld
+     on the file wx/dlimpexp.h, written by Vadim Zeitlin and published
+	 under the wxWindows licence.
+*/
+#if defined(MRPT_OS_WINDOWS)
+    /*
+       __declspec works in BC++ 5 and later, Watcom C++ 11.0 and later as well
+       as VC++ and gcc
+     */
+#    if defined(_MSC_VER) || defined(__BORLANDC__) || defined(__GNUC__) || defined(__WATCOMC__)
+#        define MRPT_OPENGL_EXPORT __declspec(dllexport)
+#        define MRPT_OPENGL_IMPORT __declspec(dllimport)
+#    else /* compiler doesn't support __declspec() */
+#        define MRPT_OPENGL_EXPORT
+#        define MRPT_OPENGL_IMPORT
+#    endif
+#elif defined(MRPT_OS_OS2)		/* was __WXPM__ */
+#    if defined (__WATCOMC__)
+#        define MRPT_OPENGL_EXPORT __declspec(dllexport)
+        /*
+           __declspec(dllimport) prepends __imp to imported symbols. We do NOT
+           want that!
+         */
+#        define MRPT_OPENGL_IMPORT
+#    elif defined(__EMX__)
+#        define MRPT_OPENGL_EXPORT
+#        define MRPT_OPENGL_IMPORT
+#    elif (!(defined(__VISAGECPP__) && (__IBMCPP__ < 400 || __IBMC__ < 400 )))
+#        define MRPT_OPENGL_EXPORT _Export
+#        define MRPT_OPENGL_IMPORT _Export
+#    endif
+#elif defined(MRPT_OS_APPLE)
+#    ifdef __MWERKS__
+#        define MRPT_OPENGL_EXPORT __declspec(export)
+#        define MRPT_OPENGL_IMPORT __declspec(import)
+#    endif
+#elif defined(__CYGWIN__)
+#    define MRPT_OPENGL_EXPORT __declspec(dllexport)
+#    define MRPT_OPENGL_IMPORT __declspec(dllimport)
+#endif
+
+/* for other platforms/compilers we don't anything */
+#ifndef MRPT_OPENGL_EXPORT
+#    define MRPT_OPENGL_EXPORT
+#    define MRPT_OPENGL_IMPORT
+#endif
+
+/*  Macros that map to export declaration when building the DLL, to import
+	declaration if using it or to nothing at all if we are not compiling as DLL */
+#if defined(MRPT_BUILT_AS_DLL)
+#	if defined(mrpt_opengl_EXPORTS)  /* Building the DLL */
+#		define OPENGL_IMPEXP MRPT_OPENGL_EXPORT
+#	else  /* Using the DLL */
+#		define OPENGL_IMPEXP MRPT_OPENGL_IMPORT
+#	endif
+#else /* not making nor using DLL */
+#    define OPENGL_IMPEXP 
+#endif
+
+
+#endif
