44c2da187bb369d35e623ecc3bc89f97e8a19aea
Paulina Zasada <pzasada@student.agh.edu.pl>
2015-01-23 02:05:44 +0100

    Getters%setters , result Keeper changes

--- Agh/DADM/Ekg/ResultKeeper.h
+++ Agh/DADM/Ekg/ResultKeeper.h
@@ -10,6 +10,25 @@ class ResultKeeper
 		static ResultKeeper& getInstance();
        // ResultKeeper();
         int someParam;
+	//	AtrialFibrillation *atrialFibrillation;
+	//	Hrv2 *hrv2;
+	//	RPeaks *rPeaks;
+	//	HRV1 *hrv1;
+	//	HeartClass *heartClass;
+	//	ECGBaseline *ecgBaseline;
+	//	EctopicBeat *ectopicBeat;
+	//	STSegmentResult *stSegmentResult;
+	//	TWaves *tWaves;
+	//	HRT *hrt;
+	//	SleepApnea *sleepApnea;
+	//	QTDisp *qtDisp;
+	//	Input *input;
+	//	RaportGenerator *raportGenerator;
+	//	SIG_EDR *sigEDR;
+	//	Waves *waves;
+
+
+
 
 
 
@@ -21,6 +40,25 @@ class ResultKeeper
 
 		vector<double>* getRawSignalMV();
 		void setRawSignalMV(vector<double>* signal);
+
+	//	AtrialFibrillation* getAtrialFibrillation();
+	//	Hrv2* getHrv2();
+	//	RPeaks* getRPeaks();
+	//	HRV1* getHRV1();
+	//	HeartClass* getHeartClass();
+	//	ECGBaseline* getECGBaseline();
+	//	EctopicBeat* getEctopicBeat();
+	//	STSegmentResult* getSTSegmentResult();
+	//	TWaves* getTWaves();
+	//	HRT* getHRT();
+	//	SleepApnea* getSleepApnea();
+	//	QTDisp* getQTDisp();
+	//	Input* getInput();
+	//	RaportGenerator* getRaportGenerator();
+	//	SIG_EDR* getSIG_EDR();
+	//	Waves* getWaves();
+
+
 
         
     private:
--- Agh/DADM/Ekg/ReturnType.h
+++ Agh/DADM/Ekg/ReturnType.h
@@ -3,8 +3,21 @@
 	enum returnTypes{
 		BASELINE,
 		HEART_CLASS,
+		HRV1e,
 		HRV2,
-		RPEAKS
+		RPEAKS,
+		ECTOPIC_BEAT,
+		ARTIAL_FIBR,
+		HRTe,
+		SIG_EDRe,
+		ST_SEGMENT,
+		T_WAVES_ALT,
+		WAVES,
+		SLEEP_APNEA,
+		QT_DISP,
+		INPUT,
+		RAPORT
+
 	}; 
 
 
