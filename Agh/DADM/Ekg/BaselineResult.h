f42b4aa1045bd6477d30c0b04f717138e3cfe87b
Paulina Zasada <pzasada@student.agh.edu.pl>
2015-01-31 20:51:59 +0100

    Opakowannie ecgBaseline

---
+++ Agh/DADM/Ekg/BaselineResult.h
@@ -0,0 +1,17 @@
+#include <AbstractResult.h>
+#include <vector>
+
+
+class BaselineResult: public AbstractResult
+{
+public:
+	BaselineResult* getResult();
+	BaselineResult();
+    ~BaselineResult();
+protected:
+	std::vector <double> filteredSignal;
+public:
+	std::vector <double> getFilteredSignal();
+	void setFilteredSignal(std::vector <double>);
+	
+};
---
+++ Agh/DADM/Ekg/ECGBaseline.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "AbstractModule.h"     
+#include "Result.h"   
+
+#include "Methods.h"
+#include "FilterType.h"
+#include "MovingAverage.h"
+#include "Butterworth.h"
+#include "Chebyshev.h"
+#include "LeastMeanSquares.h"
+#include "CubicSpline.h"
+#include "SavitzkyGolay.h"
+
+
+
+class ECGBaseline: public AbstractModule<BaselineResult> {
+
+public:
+	// BaselineResult* compute (ResultKeeper *rkp, BASELINEMETHOD baselineMethod);
+	BaselineResult* compute (ResultKeeper *rkp);
+	std::vector <double> output;
+	
+private:
+	BASELINEMETHOD baselineMethod;
+	FILTERTYPE filterType;
+	int samplingFrequency;
+	std::vector<double> butterworthFilter (std::vector<double>* signal, int samplingFrequency);
+	std::vector<double> chebyshevFilter (std::vector<double>* signal, int samplingFrequency);
+	std::vector<double> movingAverage (std::vector<double>* signal);
+	std::vector<double> leastMeanSquares (std::vector<double>* signal, int samplingFrequency);
+	std::vector<double> cubicSpline (std::vector<double>* signal, int samplingFrequency);
+	std::vector<double> savitzkyGolay (std::vector<double>* signal, int samplingFrequency);
+};
