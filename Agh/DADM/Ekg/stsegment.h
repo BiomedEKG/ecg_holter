8335b3f8a765657e5dcd86845849eb37ec702f44
Paulina Zasada <pzasada@student.agh.edu.pl>
2015-02-09 21:48:07 +0100

    STSegment poprawki

---
+++ Agh/DADM/Ekg/stsegment.h
@@ -0,0 +1,76 @@
+#ifndef STSEGMENT_H
+#define STSEGMENT_H
+
+
+#include <vector>
+#include <iterator>
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <string>
+#include <fstream>
+#include <map>
+#include "stsegmentresult.h"
+#include "AbstractModule.h"
+
+using namespace std;
+
+
+class STSegment : public AbstractModule<STSegmentResult>
+{
+	private:
+
+	vector<unsigned int> QRSonset ; 
+	vector<unsigned int> QRSend;
+	vector<double> Signal;
+	vector<unsigned int> Tpeak;
+	vector<unsigned int> Rpeak;
+	int SizeVector;
+	double Frequency;
+
+
+	vector<double> HeartRate;
+	vector<unsigned int> J20;
+	vector<unsigned int> Ton;
+	vector<unsigned int> TE;
+	vector<unsigned int> TMax;
+	vector <double> SlopeTpeak;
+	vector <double> SlopeTon;
+	vector <double> SlopeSKST;
+	vector <double> MaxDistanceTpeak;
+	vector <double> MaxDistanceTon;
+	vector <double> MaxDistanceShapeSKST;
+	vector<string> OffsetLevel;
+	vector<string> ShapeST;
+	vector <string> TypeShapeST;
+	double k1offset;
+	double k2offset;
+	double k1slope;
+	double k2slope;
+	double threshold;
+
+
+	vector<unsigned int>  computeJ20 ();
+	vector<double>  computeSlope (vector<unsigned int> TE);
+	vector<unsigned int> computeMaxDistanceIndex (vector <double> Slope,  vector<unsigned int> TE,vector<double>& MaxDistance);
+	vector<string> defineOffsetLevel ();
+	vector<double> correctSlopeorMaxDistanceForShapeST(vector<double> StraightTpeak,vector <double> StraighTon);
+	vector<unsigned int> correctTEForShapeST ();
+	vector<string>  defineShapeST ();
+	vector<string>  defineTypeShapeST ();
+	vector <double> computeHeartRate ();
+	void CorrectSize();
+	void Run();
+
+	public:
+  
+
+	STSegmentResult* compute(ResultKeeper *rkp);
+
+	
+															
+};
+
+
+#endif
---
+++ Agh/DADM/Ekg/stsegmentresult.cpp
@@ -0,0 +1,13 @@
+#include "stsegmentresult.h"
+
+STSegmentResult::STSegmentResult(void)
+{
+}
+//
+STSegmentResult::~STSegmentResult(void)
+{
+}
+STSegmentResult* STSegmentResult:: getResult() {
+//	//paramType = ;
+	return this;
+	}
