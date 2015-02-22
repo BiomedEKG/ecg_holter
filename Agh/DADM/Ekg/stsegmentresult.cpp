4b829e9a43c53d112729c0fd3fd1d4ad3d2b2fca
Paulina Zasada <pzasada@student.agh.edu.pl>
2015-02-22 16:23:09 +0100

    odkomentowane vcgLoop i stsegment

--- Agh/DADM/Ekg/stsegmentresult.cpp
+++ Agh/DADM/Ekg/stsegmentresult.cpp
@@ -1,13 +1,18 @@
-//#include "stsegmentresult.h"
+#include "stsegmentresult.h"
+
+STSegmentResult::STSegmentResult(void)
+{
+}
 //
-//STSegmentResult::STSegmentResult(void)
-//{
-//}
-////
-//STSegmentResult::~STSegmentResult(void)
-//{
-//}
-//STSegmentResult* STSegmentResult:: getResult() {
-////	//paramType = ;
-//	return this;
-//	}
+STSegmentResult::~STSegmentResult(void)
+{
+}
+STSegmentResult* STSegmentResult:: getResult() {
+	STSegmentResult *sts = new STSegmentResult();
+	sts->OffsetLevel = this->OffsetLevel;
+	sts->ShapeST = this->ShapeST;
+	sts->TypeShapeST = this->TypeShapeST;
+	sts->Ton = this->Ton;
+	sts->QRSend = this->QRSend;
+	return sts;
+	}
